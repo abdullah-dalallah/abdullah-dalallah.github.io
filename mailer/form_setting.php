fe5aea34d3ab781f0560aff58360126d374a3154
abdullhali53 <68693890+abdullhali53@users.noreply.github.com>
2022-07-07 12:41:37 +0400

    Initial Upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from Website"; //letter subject
+	$to = 'abdullhali53@gmail.com'; // Enter Your E-mail
+	$from = 'admin@you-site-name.com'; // Admin e-mail
+	$fromName = ''; // Your company name
+	$charset = 'UTF-8';
+?>
